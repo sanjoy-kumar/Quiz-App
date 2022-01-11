977545ae984380579122aa4ec060204e2f66c5a3
Shivani Sharma <shivani1406sharma@gmail.com>
2022-01-11 16:14:07 +0000

    added result route

--- routes/results.js
+++ routes/results.js
@@ -4,22 +4,30 @@ const router = express.Router();
 module.exports = (db) => {
 
 // viewing results from the quiz
-
   router.get("/:quiz_id/result", (req, res) => {
+    console.log("test");
     let string = `
-    SELECT quizzes.name, users.username, results.score
+    SELECT quizzes.name, users.name, results.score, question, user_answer
     FROM results
     JOIN quizzes ON results.quiz_id = quizzes.id
     JOIN users ON users.id = results.user_id
-    WHERE quiz_id = $1
-    ORDER BY id;
+    JOIN answers ON answers.user_id=results.user_id
+    JOIN questions ON questions.id = answers.question_id
+    WHERE quizzes.id = $1
+    ORDER BY results.id;
     `;
     db.query(string, [req.params.quiz_id])
     .then(data => {
-      let templateVar = {attempt: data.rows}
+      let templateVar = {quiz_result: data.rows}
       res.render("../views/result", templateVar);
     })
-  })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+
+  });
 
   // viewing users result from the quiz
 
@@ -29,7 +37,7 @@ module.exports = (db) => {
     FROM results
     JOIN quizzes ON results.quiz_id = quizzes.id
     JOIN users ON users.id = results.user_id
-    WHERE quiz_id = $1 and user_id = $2
+    WHERE quiz_id = $1 and quizzes.user_id = $2
     ORDER BY id;
     `;
     db.query(string, [req.params.quiz_id, req.params.user_id])
@@ -37,7 +45,7 @@ module.exports = (db) => {
       let templateVar = {attempt: data.rows}
       res.render("../views/result", templateVar);
     })
-  })
+  });
 
 
 
