c39ac8866038218f099adaf21177e15aadd04bce
Josue <arevalo.josued@gmail.com>
2022-01-13 22:09:31 +0000

    Fixing bug where main page would should private quizzes

--- routes/index.js
+++ routes/index.js
@@ -15,17 +15,18 @@ module.exports = (db,app) => {
     // const user_id = req.session.user_id;
     Promise.all([db.query(`SELECT * FROM users WHERE name = 'Josue';`),
     db.query('SELECT * FROM quizzes;'),
-    db.query('SELECT results.*, count(questions.question) as total_score, quizzes.name FROM results JOIN quizzes ON results.quiz_id = quizzes.id JOIN questions ON questions.quiz_id = results.quiz_id WHERE results.user_id = 3 GROUP BY results.id, quizzes.name LIMIT 1;')])
+    db.query('SELECT results.*, count(questions.question) as total_score, quizzes.name FROM results JOIN quizzes ON results.quiz_id = quizzes.id JOIN questions ON questions.quiz_id = results.quiz_id WHERE results.user_id = 3 GROUP BY results.id, quizzes.name LIMIT 1;'), db.query("SELECT * FROM quizzes WHERE quiz_type = 't';")])
     .then(data => {
       const userData = data[0];
       const quizData = data[1];
       const resultData = data[2];
+      const public = data[3];
 
       const templateVars = {
         user: userData.rows[0],
         featuredQuiz: quizData.rows[0],
-        quizzes: quizData.rows.slice(1),
         result: resultData.rows[0],
+        publicQuizzes: public.rows.slice(1),
       };
       delete req.session.user_id;
       req.session.user_id = templateVars.result.user_id;
